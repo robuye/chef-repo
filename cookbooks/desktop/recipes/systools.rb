5a35e9d27476d1ab1eb87c740ca5e5653d91a32a
robuye <rulejczyk@gmail.com>
2017-01-03 09:49:11 +0100

    Install vagrant and virtualbox

--- cookbooks/desktop/recipes/systools.rb
+++ cookbooks/desktop/recipes/systools.rb
@@ -16,3 +16,17 @@ package "google-chrome-stable"
 package "liblua5.2-dev"
 package "lua5.2"
 package "libjson-pp-perl"
+
+# Vagrant
+remote_file "#{Chef::Config[:file_cache_path]}/vagrant.deb" do
+  source "https://releases.hashicorp.com/vagrant/1.9.1/vagrant_1.9.1_x86_64.deb"
+  notifies :install, "dpkg_package[vagrant]", :immediately
+  not_if "dpkg -s vagrant" # package is already installed
+end
+
+dpkg_package 'vagrant' do
+  source "#{Chef::Config[:file_cache_path]}/vagrant.deb"
+  action :nothing
+end
+
+package "virtualbox"
